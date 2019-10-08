90f297a3b8f7769b9f61c9a75c819200f0a21626
Harsha Sagar <mailtoharshasagar@gmail.com>
2019-10-08 23:41:02 +0530

    connecting component to redux-store - subscribing to necessary scope of state information - registering to dispatch an action

--- src/components/containers/CakeContainer.js
+++ src/components/containers/CakeContainer.js
@@ -1,12 +1,50 @@
 import React from 'react'
 
-function CakeContainer() {
+import { connect } from 'react-redux'
+/*
+  step #1: connect is helpful to make a component:
+  - subscribing to changes in redux-store
+  - dispatching an action
+*/
+
+import { buyCake } from '../../redux/cake/cakeActions'
+
+function CakeContainer(props) {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy Cake</button>
+      <h2>Number of cakes - { props.numberOfCakes }</h2>
+      <button onClick= { props.buyCake } >Buy Cake</button>
     </div>
   )
 }
 
-export default CakeContainer
+const mapStateToProps = (state) => {
+  return {
+    numberOfCakes: state.numberOfCakes
+  }
+}
+/* 
+  step #2: subscribing to a scope of redux-store state that component is interested in
+  - Availing the necessary state to the component via props
+*/
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => {
+      dispatch(buyCake());
+    }
+  }
+}
+/* 
+  step #2: information pertaining to an action that could be dispatched by a component
+  - Availing the component to dispatch an action via props
+*/
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
+/* 
+  step #3: connecting the component to redux-store:
+  - lists the necessary state of information that component is interested in.
+  - registered for to dispatch an action
+*/
+
+// a component 'connect'ed to redux-store is said to be container.
